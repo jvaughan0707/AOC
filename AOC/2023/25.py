ee5ce89d66039188b0a405bad23b4e9fee4448ac
Josh Vaughan <josh.vaughan@cityfibre.com>
2026-01-09 11:56:58 +0000

    2023 day 25

---
+++ AOC/2023/25.py
@@ -0,0 +1,39 @@
+from AOC.shared.utils import getInput, getGlobalMinCuts, getConnectedPoints
+import sys
+sys.setrecursionlimit(150000)
+
+lines = getInput(25)
+
+adjMap = {}
+
+def createOrGet(node):
+    if node not in adjMap:
+        adjMap[node] = []
+    return adjMap[node]
+
+for line in lines:
+    source, targets = line.split(': ')
+
+    createOrGet(source).extend(targets.split())
+
+    for target in targets.split():
+        createOrGet(target).append(source)
+
+
+# cuts = ldl-fpg, dfk-nxk, hcf-lhn
+
+minCutEdges = getGlobalMinCuts(adjMap, 3)
+print(minCutEdges)
+
+for e in minCutEdges:
+    adjMap[e[0]].remove(e[1])
+    adjMap[e[1]].remove(e[0])
+
+start = list(adjMap)[0]
+
+size = len(getConnectedPoints(adjMap, start))
+
+print(size)
+print(len(adjMap) - size)
+
+print(size * (len(adjMap) - size))
--- AOC/shared/utils.py
+++ AOC/shared/utils.py
@@ -1,19 +1,20 @@
+from copy import deepcopy
 
 
-def expandWithPlaceholders(values, expandFunc, iterations, sizeFunc = None):
+def expandWithPlaceholders(values, expandFunc, iterations, sizeFunc=None):
     expansionMap = {}
     if sizeFunc is None:
         sizeFunc = lambda x: len(x)
 
     class Item:
-        def __init__(self, value, parent = None):
+        def __init__(self, value, parent=None):
             self.value = value
             self.parent = parent
 
         def __repr__(self):
             return str(self.value)
 
-        def updateParents(self, depth = 2):
+        def updateParents(self, depth=2):
             current = self.parent
 
             while current.parent:
@@ -46,7 +47,7 @@ def expandWithPlaceholders(values, expandFunc, iterations, sizeFunc = None):
     for v in values:
         items.append(Item(v))
 
-    def recurse(currentItems, i = 1):
+    def recurse(currentItems, i=1):
         newItems = []
         totalLength = 0
         for item in currentItems:
@@ -77,6 +78,7 @@ def expandWithPlaceholders(values, expandFunc, iterations, sizeFunc = None):
 
     return recurse(items)
 
+
 def flipGrid(grid):
     newRows = []
 
@@ -93,25 +95,32 @@ def flipGrid(grid):
             newRows.append(newRow)
     return newRows
 
-def sub (p1, p2):
+
+def sub(p1, p2):
     return tuple(p1[i] - p2[i] for i in range(len(p1)))
+
 
 def add(p1, p2):
     return tuple(p1[i] + p2[i] for i in range(len(p1)))
 
+
 def scale(p, s):
     return tuple(p[i] * s for i in range(len(p)))
+
 
 def dot(p1, p2):
     return p1[0] * p2[0] + p1[1] * p2[1]
 
+
 def isOob(grid, p):
-    return not(0 <= p[0] < len(grid) and 0 <= p[1] < len(grid[0]))
+    return not (0 <= p[0] < len(grid) and 0 <= p[1] < len(grid[0]))
+
 
 def get(grid, p):
     if isOob(grid, p):
         return None
     return grid[p[0]][p[1]]
+
 
 up = (-1, 0)
 down = (1, 0)
@@ -124,9 +133,11 @@ directions = {
     'r': right
 }
 
+
 def getInput(day):
     with open(f'inputs/{day}') as f:
         return f.read().splitlines()
+
 
 def getGridInput(day):
     lines = getInput(day)
@@ -137,6 +148,7 @@ def getGridInput(day):
 
     return grid
 
+
 def getFirstInGrid(grid, char):
     width = len(grid[0])
     height = len(grid)
@@ -145,6 +157,7 @@ def getFirstInGrid(grid, char):
         for j in range(width):
             if grid[i][j] == char:
                 return i, j
+
 
 def getMinDist(adjacencyMatrix, start, end):
     distMap = {x: float('inf') for x in adjacencyMatrix}
@@ -174,7 +187,8 @@ def getMinDist(adjacencyMatrix, start, end):
     # print(distMap)
     return distMap[end]
 
-def visualiseGraph(adjacencyMatrix, directed = False, weighted = False, colorMap = None, name = 'graph'):
+
+def visualiseGraph(adjacencyMatrix, directed=False, weighted=False, colorMap=None, name='graph'):
     from pyvis.network import Network
     import os
     import webbrowser
@@ -199,7 +213,8 @@ def visualiseGraph(adjacencyMatrix, directed = False, weighted = False, colorMap
     net.show(name, notebook=False)
     webbrowser.open('file://' + os.path.realpath(name))
 
-def getNetwork(grid, adjFunc = None, wallSymbol = '#'):
+
+def getNetwork(grid, adjFunc=None, wallSymbol='#'):
     nodes = {}
     width = len(grid[0])
     height = len(grid)
@@ -208,12 +223,119 @@ def getNetwork(grid, adjFunc = None, wallSymbol = '#'):
             current = grid[i][j]
             if current == wallSymbol:
                 continue
-            nodes[(i,j)] = []
+            nodes[(i, j)] = []
             for direction in directions:
-                neighbourPoint = add((i,j), directions[direction])
+                neighbourPoint = add((i, j), directions[direction])
                 neighbourSymbol = get(grid, neighbourPoint)
                 if not neighbourSymbol or neighbourSymbol == wallSymbol:
                     continue
                 if not adjFunc or adjFunc(current, neighbourPoint, direction):
-                    nodes[(i,j)].append(neighbourPoint)
+                    nodes[(i, j)].append(neighbourPoint)
     return nodes
+
+
+def getPathDfs(adjMap, start, end, currentPath=None):
+    if currentPath is None:
+        currentPath = [start]
+    if end in adjMap[start]:
+        currentPath.append(end)
+        return currentPath
+    for neighbour in adjMap[start]:
+        if neighbour not in currentPath:
+            path = getPathDfs(adjMap, neighbour, end, currentPath + [neighbour])
+
+            if path:
+                return path
+
+    return None
+
+def getPathBfs(adjMap, start, end):
+    nodeDists = { 0: [start]}
+    visited = {start}
+    while True:
+        maxDist = max(nodeDists.keys())
+        nodeDists[maxDist + 1] = []
+
+        if end in nodeDists[maxDist]:
+            break
+        for node in nodeDists[maxDist]:
+            for neighbour in adjMap[node]:
+                if neighbour not in visited:
+                    nodeDists[maxDist + 1].append(neighbour)
+                    visited.add(neighbour)
+
+        if not nodeDists[maxDist + 1]:
+            return None
+
+    dist = maxDist - 1
+    current = end
+    path = [end]
+
+    while dist >= 0:
+        for neighbour in nodeDists[dist]:
+            if current in adjMap[neighbour]:
+                path.append(neighbour)
+                current = neighbour
+                dist -= 1
+                break
+
+    path.reverse()
+    return path
+
+def getConnectedPoints(adjMap, start, currentSet = None):
+    if currentSet is None:
+        currentSet = set()
+    for neighbour in adjMap[start]:
+        if neighbour not in currentSet:
+            currentSet.add(neighbour)
+            getConnectedPoints(adjMap, neighbour, currentSet)
+
+    return currentSet
+
+def getMinCuts(adjMap, s, t, upperLimit):
+    cuts = 0
+
+    adjMapCopy = deepcopy(adjMap)
+
+    while cuts <= upperLimit:
+        path = getPathBfs(adjMapCopy, s, t)
+
+        if not path:
+            break
+
+        cuts += 1
+
+        for i in range(len(path) - 1):
+            n1 = path[i]
+            n2 = path[i + 1]
+            adjMapCopy[n1].remove(n2)
+
+    if cuts > upperLimit:
+        return None
+
+    S = getConnectedPoints(adjMapCopy, s)
+
+    cutEdges = []
+
+    for x in adjMap:
+        for y in adjMap[x]:
+            if x in S and y not in S:
+                cutEdges.append((x, y))
+
+    return cutEdges
+
+def getGlobalMinCuts(adjMap, upperLimit):
+    for n1 in adjMap:
+        for n2 in adjMap:
+            if n1 == n2:
+                continue
+
+            if n1 in adjMap[n2]:
+                continue
+
+            minCuts = getMinCuts(adjMap, n1, n2, upperLimit)
+
+            if minCuts:
+                return minCuts
+
+    return None
